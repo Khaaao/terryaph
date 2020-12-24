ae5e0227511674ab5bff5b86944df7822ffc9342
Kevin Khao <khao.kevin@gmail.com>
2020-12-24 11:33:12 +0100

    fix exo 13

--- exo13.c
+++ exo13.c
@@ -10,7 +10,7 @@ void swap (int *a, int *b)
 int main (){
     int a = 1, b = 10;
     printf("Avant a= %d / b = %d", a, b);
-    PERMUTER(&a, &b);
+    swap(&a, &b);
     printf("\nApres a = %d / b = %d", a, b);
     return 0;
 }
